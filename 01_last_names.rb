e542972aaf53f624aea1cf805311519bf94a0e4d
Lindsay-R <lrecknell@gmail.com>
2014-06-27 09:47:17 -0600

    story1 done

--- 01_last_names.rb
+++ 01_last_names.rb
@@ -1,3 +1,8 @@
 require_relative 'people'
 
 # What are the last names of all of the people?
+puts "#{PEOPLE["joe"][:last_name]} #{PEOPLE["sue"][:last_name]}"
+
+
+
+PEOPLE.each_key { |key| puts PEOPLE[key][:last_name] }
